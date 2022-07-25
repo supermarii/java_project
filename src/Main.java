08be40649b3b7774ebee81f82c9f41ff95615303
supermarii <marius.kammer@gmx.net>
2022-07-25 19:54:39 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         final int x = 10;
         final int y = 12;
+        final int z = 23;
         System.out.println("test");
     }
 }
