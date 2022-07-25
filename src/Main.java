54e91f6bcefad35886705790052b8f3629aff6cf
Marius Kammer <64333883+supermarii@users.noreply.github.com>
2022-07-25 21:28:19 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("enter a number!!!");
+        System.out.println("Enter a number!!!");
 
         Scanner sn = new Scanner(System.in);
 
