4b8e47fb3b52be96bc5af59dcd76d435018c2aae
supermarii <marius.kammer@gmx.net>
2022-07-25 21:27:01 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        final int x = 10;
-        final int y = 12;
-        final int z = 23;
-        System.out.println("test123");
+
+        System.out.println("enter a number");
 
         Scanner sn = new Scanner(System.in);
 
         final int userInput = sn.nextInt();
-        final int userInput2 = sn.nextInt();
-        final int userInput3 = sn.nextInt();
+
         System.out.println(userInput);
 
     }
