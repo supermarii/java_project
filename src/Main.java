892dfa7fce8e04b7dfa7f6024ad526037c194a1d
supermarii <marius.kammer@gmx.net>
2022-07-25 21:51:03 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hey!");
+        System.out.println("Hey!!");
         System.out.println("Enter a nice number!!! :)");
 
         Scanner sn = new Scanner(System.in);
