2d0cfff255f4ea91c8c3099b6b936bc003e6d126
supermarii <marius.kammer@gmx.net>
2022-07-25 19:57:10 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         final int x = 10;
         final int y = 12;
         final int z = 23;
         System.out.println("test");
+
+        Scanner sn = new Scanner(System.in);
+
+        final int userInput = sn.nextInt();
+        System.out.println(userInput);
+
     }
 }
